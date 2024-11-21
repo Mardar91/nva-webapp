2620f8b029dc23148964af593677ee05094b5c1f
Mardar91 <142995846+Mardar91@users.noreply.github.com>
2024-11-21 19:05:42 +0100

    Update Partners.tsx

--- pages/Partners.tsx
+++ pages/Partners.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "/components/ui/button";
+import { Button } from "../components/ui/button";
 import VipTicketHero from "../components/VipTicketHero";
 import { Linking } from "react-native";
 

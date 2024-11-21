2b3bd9cc00560d26ed9551fce0e9d4d26847bad8
Mardar91 <142995846+Mardar91@users.noreply.github.com>
2024-11-21 20:36:11 +0100

    Delete lib directory

--- lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-// Funzione che unisce le classi con clsx e le ottimizza con twMerge
-export function cn(...inputs: ClassValue[]): string {
-  return twMerge(clsx(inputs));
-}

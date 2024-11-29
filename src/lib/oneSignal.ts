1f9759e3b999a6810260ebb9f23abfa756c17987
Mardar91 <142995846+Mardar91@users.noreply.github.com>
2024-11-30 00:06:17 +0100

    Update oneSignal.ts

--- src/lib/oneSignal.ts
+++ src/lib/oneSignal.ts
@@ -6,7 +6,7 @@ import OneSignal from "react-onesignal";
 export const initOneSignal = async () => {
   try {
     await OneSignal.init({
-      appId: "8d05cb31-99c9-4dd2-a2a9-8e7fb838fb8a", // Il tuo appId
+      appId: "8d05cb31-99c9-4dd2-a2a9-8e7fb838fb8a", // Sostituisci con il tuo APP_ID
       notifyButton: {
         enable: true,
         position: "bottom-right",
@@ -16,11 +16,12 @@ export const initOneSignal = async () => {
 
     console.log("OneSignal initialized successfully!");
 
-    // Verifica lo stato delle notifiche
-    const subscriptionStatus = await OneSignal.User.isSubscribed();
-    console.log("Stato sottoscrizione notifiche:", subscriptionStatus);
+    // Verifica lo stato delle notifiche push
+    const isPushEnabled = await OneSignal.Notifications.isPushNotificationsEnabled();
+    console.log("Notifiche push abilitate:", isPushEnabled);
 
-    if (!subscriptionStatus) {
+    if (!isPushEnabled) {
+      // Mostra il prompt
       await OneSignal.Slidedown.open();
     }
   } catch (error) {

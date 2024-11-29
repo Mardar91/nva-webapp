1869ecb8fa225bda42d5ccb5026303aec92ec2b0
Mardar91 <142995846+Mardar91@users.noreply.github.com>
2024-11-29 23:59:48 +0100

    Update oneSignal.ts

--- src/lib/oneSignal.ts
+++ src/lib/oneSignal.ts
@@ -6,16 +6,21 @@ import OneSignal from "react-onesignal";
 export const initOneSignal = async () => {
   try {
     await OneSignal.init({
-      appId: "8d05cb31-99c9-4dd2-a2a9-8e7fb838fb8a", // Sostituisci con il tuo APP_ID
+      appId: "8d05cb31-99c9-4dd2-a2a9-8e7fb838fb8a", // Il tuo appId
       notifyButton: {
         enable: true,
         position: "bottom-right",
       },
-      // Per test in localhost
       allowLocalhostAsSecureOrigin: process.env.NODE_ENV === "development",
     });
 
     console.log("OneSignal initialized successfully!");
+
+    // Mostra il prompt per abilitare le notifiche
+    const isSubscribed = await OneSignal.isPushNotificationsEnabled();
+    if (!isSubscribed) {
+      await OneSignal.showSlidedownPrompt();
+    }
   } catch (error) {
     console.error("Errore durante l'inizializzazione di OneSignal:", error);
   }

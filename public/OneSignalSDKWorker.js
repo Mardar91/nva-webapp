65e27177e701cf84daa1decba50e00c8cdb3d064
Mardar91 <142995846+Mardar91@users.noreply.github.com>
2024-11-30 12:24:23 +0100

    Update OneSignalSDKWorker.js

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,1 +1,6 @@
+// Import the OneSignal SDK worker
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+// Import your PWA service worker
+importScripts("/pwa-worker.js");
+

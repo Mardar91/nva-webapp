e7a3418b7c7ab9d41aee0fdf764345766b54ca54
Mardar91 <142995846+Mardar91@users.noreply.github.com>
2024-12-02 23:14:01 +0100

    Update schedule-notification.ts

--- src/pages/api/schedule-notification.ts
+++ src/pages/api/schedule-notification.ts
@@ -8,6 +8,20 @@ export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
+  // Abilita CORS
+  response.setHeader('Access-Control-Allow-Credentials', 'true');
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  response.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  if (request.method === 'OPTIONS') {
+    response.status(200).end();
+    return;
+  }
+
   if (request.method !== 'POST') {
     return response.status(405).json({ message: 'Method not allowed' });
   }

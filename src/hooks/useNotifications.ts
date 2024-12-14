9fc862570d2cf01c1edbec094d76de153f4bf18e
Mardar91 <142995846+Mardar91@users.noreply.github.com>
2024-12-14 14:16:17 +0100

    Update useNotifications.ts

--- src/hooks/useNotifications.ts
+++ src/hooks/useNotifications.ts
@@ -5,17 +5,18 @@ import {
   getSubscriptionStatus,
   requestNotificationPermission,
   isOneSignalAvailable,
-  optInToNotifications
+    optInToNotifications
 } from '../lib/notifications/oneSignal';
 import { initializeCheckInNotifications } from '../lib/notifications/checkInNotifications';
 
 export const useNotifications = () => {
-  const [isSupported, setIsSupported] = useState(false);
-  const [isSubscribed, setIsSubscribed] = useState(false);
-  const [deviceId, setDeviceId] = useState<string | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
+    const [isSupported, setIsSupported] = useState(false);
+    const [isSubscribed, setIsSubscribed] = useState(false);
+    const [deviceId, setDeviceId] = useState<string | null>(null);
+    const [error, setError] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
     const isInitializedRef = useRef(false);
+    const timerRef = useRef<NodeJS.Timeout | null>(null); // Ref per controllare il timer
 
     useEffect(() => {
         const checkSupport = async () => {
@@ -23,8 +24,8 @@ export const useNotifications = () => {
                 if (isOneSignalAvailable()) {
                     setIsSupported(true);
                    if (!isInitializedRef.current){
-                        await initializeCheckInNotifications();
-                        isInitializedRef.current = true;
+                        await initializeCheckInNotifications(timerRef);
+                       isInitializedRef.current = true;
                     }
                     setIsLoading(false); // Imposta a false al termine dell'inizializzazione
                 } else {
@@ -58,16 +59,16 @@ export const useNotifications = () => {
     }, [isSupported]);
 
     const requestPermission = async () => {
-      if (!isSupported) {
-          setError('Notifications are not supported');
-          return;
-      }
+        if (!isSupported) {
+            setError('Notifications are not supported');
+            return;
+        }
 
         try {
             const permission = await requestNotificationPermission();
             if (permission){
-              setIsSubscribed(true);
-              await optInToNotifications();
+                setIsSubscribed(true);
+                await optInToNotifications();
             }
         } catch (err) {
             setError('Error requesting notification permission.');
@@ -79,6 +80,7 @@ export const useNotifications = () => {
         deviceId,
         error,
         requestPermission,
-        isLoading
+        isLoading,
+        timerRef
     };
 };
